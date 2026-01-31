50c173dd2bc3e9c3a58b2dabd9ef05a66ce263b5
Clayton Santos <claytondesousasantos@gmail.com>
2026-01-31 16:02:46 -0300

    ajustando model task

--- internal/model/task.go
+++ internal/model/task.go
@@ -3,9 +3,11 @@ package model
 import "time"
 
 type Task struct {
-	ID          int64     `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Done        bool      `json:"done"`
-	CreatedAt   time.Time `json:"created_at"`
+	ID          int64      `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	CompletedAt *time.Time `json:"completed_at"`
+	Done        bool       `json:"done"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
 }
